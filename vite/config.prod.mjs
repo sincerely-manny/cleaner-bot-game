a1d952f5f5bb646971f39123ec5f5635e5d91e8b
Kirill Serebryannyy <sincerely.manny@gmail.com>
2024-06-06 02:08:53 +0300

    [feat] build added

--- vite/config.prod.mjs
+++ vite/config.prod.mjs
@@ -7,41 +7,41 @@ const phasermsg = () => {
             process.stdout.write(`Building for production...\n`);
         },
         buildEnd() {
-            const line = "---------------------------------------------------------";
+            const line =
+                '---------------------------------------------------------';
             const msg = `❤️❤️❤️ Tell us about your game! - games@phaser.io ❤️❤️❤️`;
             process.stdout.write(`${line}\n${msg}\n${line}\n`);
-            
+
             process.stdout.write(`✨ Done ✨\n`);
-        }
-    }
-}   
+        },
+    };
+};
 
 export default defineConfig({
     base: './',
     logLevel: 'warning',
     build: {
+        outDir: 'docs',
         rollupOptions: {
             output: {
                 manualChunks: {
-                    phaser: ['phaser']
-                }
-            }
+                    phaser: ['phaser'],
+                },
+            },
         },
         minify: 'terser',
         terserOptions: {
             compress: {
-                passes: 2
+                passes: 2,
             },
             mangle: true,
             format: {
-                comments: false
-            }
-        }
+                comments: false,
+            },
+        },
     },
     server: {
-        port: 8080
+        port: 8080,
     },
-    plugins: [
-        phasermsg()
-    ]
+    plugins: [phasermsg()],
 });
